a9087dda65664da679247b7a975134c68f1c97f1
TEX <tex314@gmail.com>
2012-11-09 16:10:30 +0900

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendCode::Application.config.secret_token = 'c3e64d1dd5c828837c114704d053279705cb59a447261fc5074c5311905b84247bf958ee22101f7ccbd90295876bf5876228053c8e79528059dabd4862ad8a2c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FriendCode::Application.config.session_store :cookie_store, key: '_friend-code_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FriendCode::Application.config.session_store :active_record_store
